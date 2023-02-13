17dd61bf2154bd3c03d13d619e7162425343d66d
Roman Shulga <96079947+romfantast@users.noreply.github.com>
2023-02-13 16:21:39 +0200

    add goods page

--- app.js
+++ app.js
@@ -39,3 +39,48 @@ app.get("/", (req, res) => {
     });
   });
 });
+
+app.get("/cat", function (req, res) {
+  console.log(req.query.id);
+  let catId = req.query.id;
+
+  let cat = new Promise(function (resolve, reject) {
+    connection.query(
+      "SELECT * FROM category WHERE id=" + catId,
+      function (error, result) {
+        if (error) reject(error);
+        resolve(result);
+      }
+    );
+  });
+  let goods = new Promise(function (resolve, reject) {
+    connection.query(
+      "SELECT * FROM goods WHERE category=" + catId,
+      function (error, result) {
+        if (error) reject(error);
+        resolve(result);
+      }
+    );
+  });
+
+  Promise.all([cat, goods]).then(function (value) {
+    console.log(value[0]);
+    res.render("cat", {
+      cat: JSON.parse(JSON.stringify(value[0])),
+      goods: JSON.parse(JSON.stringify(value[1])),
+    });
+  });
+});
+
+app.get("/goods", function (req, res) {
+  console.log(req.query.id);
+  connection.query(
+    "SELECT * FROM goods WHERE id=" + req.query.id,
+    function (error, result, fields) {
+      if (error) throw error;
+      res.render("goods", {
+        goods: JSON.parse(JSON.stringify(result)),
+      });
+    }
+  );
+});
